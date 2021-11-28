5b0863c3184a65eb46311037c91d8fc40b0c74ef
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-11-28 22:17:30 +0900

    기본문법 학습

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,13 @@
-alert("hi");
+const player = {
+    name:"sangil",
+    points:10,
+    fat:true
+}
+console.log(player);
+console.log(player.name);
+player.fat = false;
+console.log(player.fat);
+player.lastName="potato"
+console.log(player.lastName);
+player.points= player.points + 15;
+console.log(player.points);
