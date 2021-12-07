567ac2a0ed651ecabb93ae031c87bbe177bc122e
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-12-07 21:12:31 +0900

    Login-start

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,9 @@
-const player = {
-    name:"sangil",
-    points:10,
-    fat:true
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function handleLoginClick() {
+    console.log("hello", loginInput.value);
 }
-console.log(player);
-console.log(player.name);
-player.fat = false;
-console.log(player.fat);
-player.lastName="potato"
-console.log(player.lastName);
-player.points= player.points + 15;
-console.log(player.points);
+
+loginButton.addEventListener("click",handleLoginClick);
